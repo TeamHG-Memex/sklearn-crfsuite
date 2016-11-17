e644d006bb495582594ab993934e5e615d9f2731
Mikhail Korobov <kmike84@gmail.com>
2016-11-17 16:41:24 +0500

    bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sklearn-crfsuite',
-    version='0.3.3',
+    version='0.3.4',
     author='Mikhail Korobov',
     author_email='kmike84@gmail.com',
     license='MIT license',
