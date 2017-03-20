41e3831ca98e75a9d9e4765572ad554f688a2a18
Mikhail Korobov <kmike84@gmail.com>
2017-03-21 03:12:22 +0500

    DOC fix sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ from unittest.mock import MagicMock
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-        return Mock()
+        return MagicMock()
 
 MOCK_MODULES = ['sklearn', 'sklearn.metrics', 'tqdm', 'tabulate', 'numpy',
                 'pycrfsuite']
